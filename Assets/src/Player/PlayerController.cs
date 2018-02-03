0edfd925138f69fe41a5add7ae57eca6a0244858
Ethan Hunter <fc.spaceman@gmail.com>
2018-02-03 14:54:21 -0700

    inital commit

---
+++ Assets/src/Player/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+
+        public Sprite sprite;
+        public float jumpHeight;
+        public float timeToMaxHeight;
+        public float timeFromMaxHeight;
+
+        private Vector2 downRayStart;
+        private Vector2 upRayStart;
+        private Vector2 leftRayStart;
+        private Vector2 rightRayStart;
+
+        private float upGravity;
+        private float downGravity;
+
+        private float jumpVelocity;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
